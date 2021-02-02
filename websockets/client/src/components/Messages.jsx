86e49b2a6b1ab7160c2eb54a6a44f59a50f795fe
jsksru <mail@jsks.ru>
2021-02-02 20:15:22 +0500

    list messages component

---
+++ websockets/client/src/components/Messages.jsx
@@ -0,0 +1,39 @@
+import PublicMessage from './PublicMessage';
+import InfoMessage from './InfoMessage';
+
+const messageType = (message, index) => {
+  if (message.type && message.type === 'MESSAGE') {
+    return <PublicMessage author={message.from} text={message.text} time={message.time} key={index}/>;
+  }
+
+  if (message.type && message.type === 'CONNECTED') {
+    return <InfoMessage key={index}>{message.user} вошел(ла) в чат</InfoMessage>;
+  }
+
+  if (message.type && message.type === 'DISCONNECTED') {
+    return <InfoMessage key={index}>{message.user} покинул(а) чат</InfoMessage>;
+  }
+
+  if (message.type && message.type === 'CLIENTS') {
+    return (
+      <div key={index}>
+        <h4>Clients:</h4>
+        <ul>
+          {message.clients.map((c,i) => <li key={i}>{c.id}<br/>{c.userAgent}<br/>{c.ip}</li>)}
+        </ul>
+      </div>
+    );
+  }
+
+  return;
+};
+
+const Messages = ({ items }) => {
+  return (
+    <div className="messages" style={{height: '400px', padding: '10px', border: '1px solid black'}}>
+      {items.map(messageType)}
+    </div>
+  );
+};
+
+export default Messages;
