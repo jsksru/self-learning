bf58ddad4e99278082ec2ccc26892f9e053f40af
jsksru <mail@jsks.ru>
2021-02-07 12:00:36 +0500

    some style

--- speech-api/src/App.jsx
+++ speech-api/src/App.jsx
@@ -1,9 +1,11 @@
 import Reader from './components/Reader';
+import Recognizer from './components/Recognizer';
 
 const App = () => {
   return (
-    <div>
+    <div style={{display: 'flex', justifyContent:'center', gap: 30, padding: 50}}>
       <Reader/>
+      <Recognizer/>
     </div>
   );
 };
